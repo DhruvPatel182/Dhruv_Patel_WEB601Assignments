1d485ae7b193c985bff295958cc7352fd3b41852
Dhruv Patel <dp32517263@gmail.com>
2022-06-13 09:41:18 -0400

    remove component html ts elements and list class components

--- Assignment-1/d_patel_IndianLiterature/src/app/models/list.ts
+++ Assignment-1/d_patel_IndianLiterature/src/app/models/list.ts
@@ -1,34 +1,5 @@
 import { Title } from "@angular/platform-browser";
 import { Content } from "./content";
 
-export class List {
-    private _items : Content[];
-
-    constructor(){
-        this._items = [];
-    }
-
-    get items(): Content[]{
-        return this._items;
-    }
-
-    addContent(oneContentItem : Content):void{
-        this._items.push(oneContentItem);
-    }
-    
-
-    getLength():number{
-    return  this._items.length;
-    }
-
-    printOutput(index:number):any{
-       return `
-       <div class='ListItem'>
-       <head><h1>${this.items[index].title}</h1></head>
-       <body><p>${this.items[index].body}</p>
-       <h4>${this.items[index].author}</h4>
-       <img src="${this.items[index].imageLink}"></img>
-       <div class='type'></div>
-       </div> `;
-    }  
+export class List {      
 }
